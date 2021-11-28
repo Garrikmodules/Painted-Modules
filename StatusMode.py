5037f16b907c87ea19c6814f110fe6a36c335337
Garrikmodules <89676306+Garrikmodules@users.noreply.github.com>
2021-11-28 19:08:00 +0500

    Update StatusMode.py

--- StatusMode.py
+++ StatusMode.py
@@ -7,6 +7,8 @@ from time import sleep
 import random
 import pyrogram
 from pyrogram import Client, filters
+def datadef():
+    return {"name":"StatusMod","help":"%status typing|upload_photo|upload_video|upload_audio|upload_document|find_location|upload_video_note|choose_contact|playing|speaking|cancel","description":"Fake status"}
 @Client.on_message(filters.command("status", prefixes = "%")&filters.me)
 def stick(client, msg):
  orig_text=msg.text.split("%status ", maxsplit = 1)[1]
